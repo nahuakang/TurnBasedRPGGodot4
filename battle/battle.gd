28724c831477faa995291c764e29142fc501833d
Nahua Kang <kangnahua@gmail.com>
2023-05-23 20:56:13 +0200

    13: (Almost) always await down, like call down, signal up

--- battle/battle.gd
+++ battle/battle.gd
@@ -2,9 +2,11 @@ extends Node2D
 
 @onready var player_battle_unit: BattleUnit = $PlayerBattleUnit/PlayerBattleUnit
 @onready var enemy_battle_unit: BattleUnit = $EnemyBattleUnit/EnemyBattleUnit
-
+@onready var pan_in_animation_player: AnimationPlayer = $AnimationPlayer
 
 func _ready() -> void:
+	await pan_in_animation_player.animation_finished
+
 	player_battle_unit.melee_attack()
 	enemy_battle_unit.melee_attack()
 
