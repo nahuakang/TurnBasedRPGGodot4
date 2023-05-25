d82a6971ea5877b5203aa3e5640c4850ab263659
Nahua Kang <n.kang@opentalk.eu>
2023-05-25 23:21:14 +0200

    35: Basic `ClassStats` resource class and two resources for Elizabeth and Werewolf

---
+++ class_stats/class_stats.gd
@@ -0,0 +1,12 @@
+extends Resource
+class_name ClassStats
+
+@export var name: String
+@export var max_health: int
+@export var attack: int
+@export var defense: int
+
+@export var battle_animations: PackedScene
+
+var level: int = 1
+var health: int = 1
