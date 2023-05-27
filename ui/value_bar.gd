aaa06045592993b22a6adc4a76c3b04f461ceaf2
Nahua Kang <kangnahua@gmail.com>
2023-05-27 10:03:33 +0200

    44-45: Health value bar creation and animation

---
+++ ui/value_bar.gd
@@ -0,0 +1,68 @@
+extends TextureRect
+
+#############
+## VARIABLES
+#############
+
+@onready var decrease_bar := $DecreaseProgressBar
+@onready var increase_bar := $IncreaseProgressBar
+@onready var actual_bar := $ActualProgressBar
+
+var bar_value: float = 0.0
+
+###########
+## SIGNALS
+###########
+
+signal animation_finished
+
+#############
+## OVERRIDES
+#############
+
+#func _ready() -> void:
+#	await animate_bar(40, 60)
+#	await animate_bar(30, 60)
+#	await animate_bar(50, 60)
+#	await animate_bar(45, 60)
+
+###########
+## METHODS
+###########
+
+func set_bar(value: float, max_value: float) -> void:
+	bar_value = calculate_bar_value(value, max_value)
+	decrease_bar.value = bar_value
+	increase_bar.value = bar_value
+	actual_bar.value = bar_value
+
+
+func animate_bar(value: float, max_value: float, duration: float = 1.0) -> void:
+	var previous_bar_value := bar_value
+	bar_value = calculate_bar_value(value, max_value)
+
+	var tween := create_tween().set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+
+	# Healing
+	if bar_value > previous_bar_value:
+		decrease_bar.value = bar_value
+		increase_bar.value = bar_value
+
+		tween.tween_property(actual_bar, "value", bar_value, duration).from_current()
+		await tween.finished
+
+	elif bar_value < previous_bar_value:
+		increase_bar.value = bar_value
+		actual_bar.value = bar_value
+
+		tween.tween_property(decrease_bar, "value", bar_value, duration).from_current()
+		await tween.finished
+
+	else:
+		tween.kill()
+
+	animation_finished.emit()
+
+
+func calculate_bar_value(value: float, max_value: float) -> float:
+	return (value / max_value) * 100.0
