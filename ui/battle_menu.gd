02edb76b478e71908bcb1506bb933b327ea48c66
Nahua Kang <kangnahua@gmail.com>
2023-05-28 14:47:28 +0200

    56: Create `BattleMenu` script
    Using https://www.youtube.com/watch?v=whd1oCWSXLI to fix the blurry
    pixel look (Set Project Settings -> Render -> Textures -> Canvas Textures
    -> Default Texture Filter to Nearest).

--- ui/battle_menu.gd
+++ ui/battle_menu.gd
@@ -1,4 +1,49 @@
 extends HBoxContainer
+class_name BattleMenu
+
+#############
+## VARIABLES
+#############
+
 @onready var action_button: TextureButton = %ActionButton
 @onready var item_button: TextureButton = %ItemButton
 @onready var run_button: TextureButton = %RunButton
+
+#########
+## ENUMS
+#########
+
+enum MENU_OPTION {
+	ACTION,
+	ITEM,
+	RUN
+}
+
+###########
+## SIGNALS
+###########
+
+signal menu_option_selected(option: MENU_OPTION)
+
+#############
+## OVERRIDES
+#############
+
+func _ready() -> void:
+	action_button.grab_focus()
+
+
+##################
+## SIGNAL METHODS
+##################
+
+func _on_action_button_button_down():
+	menu_option_selected.emit(MENU_OPTION.ACTION)
+
+
+func _on_item_button_button_down():
+	menu_option_selected.emit(MENU_OPTION.ITEM)
+
+
+func _on_run_button_button_down():
+	menu_option_selected.emit(MENU_OPTION.RUN)
