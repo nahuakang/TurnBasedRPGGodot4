122a14ad84cf13daadd151d243c006b16ae29d02
Nahua Kang <kangnahua@gmail.com>
2023-06-05 21:11:57 +0200

    82: Stumpy Quest

--- world/stumpy_npc.gd
+++ world/stumpy_npc.gd
@@ -1,14 +1,51 @@
 extends NPC
 
+#############
+## CONSTANTS
+#############
+
+const ELIZABETH_CHARACTER: Character = preload("res://characters/elizabeth_character.tres")
+const APPLE_ITEM: Item = preload("res://items/apple_item.tres")
+
+#############
+## VARIABLES
+#############
+
+var inventory := ReferenceStash.inventory
+var has_apple := false
+
+#############
+## OVERRIDES
+#############
 
 func _run_interaction() -> void:
-	Events.request_show_dialog.emit("Aaaahhh! I have a headache.", character)
-	await Events.dialog_finished
+	if not has_apple:
+		Events.request_show_dialog.emit("Aaaahhh! I have a headache.", character)
+		await Events.dialog_finished
 
-	Events.request_show_dialog.emit("Can you find a potion?", character)
-	await Events.dialog_finished
+		Events.request_show_dialog.emit("Do you have an apple I can eat?", character)
+		await Events.dialog_finished
 
-	Events.request_show_dialog.emit(
-		"I'll look around for you.",
-		load("res://characters/elizabeth_character.tres"))
-	await Events.dialog_finished
+		var apple := inventory.remove_item(APPLE_ITEM)
+		if apple is Item:
+			Events.request_show_dialog.emit("I do.", ELIZABETH_CHARACTER)
+			await Events.dialog_finished
+
+			Events.request_show_dialog.emit("Here it is.", ELIZABETH_CHARACTER)
+			await Events.dialog_finished
+
+			has_apple = true
+
+		else:
+			Events.request_show_dialog.emit("I'll look around for you.", ELIZABETH_CHARACTER)
+			await Events.dialog_finished
+
+	if has_apple:
+		Events.request_show_dialog.emit("I feel much better now. ", character)
+		await Events.dialog_finished
+
+		Events.request_show_dialog.emit("Thank you!", character)
+		await Events.dialog_finished
+
+		Events.request_show_dialog.emit("You're welcome.", ELIZABETH_CHARACTER)
+		await Events.dialog_finished
