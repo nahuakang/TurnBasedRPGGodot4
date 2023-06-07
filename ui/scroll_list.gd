c68a078fea6de7f017da6bf1629f30bc0e0c5b9a
Nahua Kang <kangnahua@gmail.com>
2023-06-07 20:50:27 +0200

    92: Fill the `ScrollList` with buttons with resources

--- ui/scroll_list.gd
+++ ui/scroll_list.gd
@@ -2,25 +2,54 @@ extends PanelContainer
 class_name ScrollList
 
 #############
+## CONSTANTS
+#############
+
+const RESOURCE_BUTTON_SCENE: PackedScene = preload("res://ui/resource_button.tscn")
+
+#############
 ## VARIABLES
 #############
 
-@onready var resource_button: ResourceButton = $MarginContainer/ScrollContainer/MarginContainer/ButtonContainer/ResourceButton
 @onready var scroll_container: ScrollContainer = %ScrollContainer
 @onready var button_container: VBoxContainer = %ButtonContainer
+
+var elizabeth_stats: PlayerClassStats = ReferenceStash.elizabeth_stats
 
 #############
 ## OVERRIDES
 #############
 
 func _ready():
-	resource_button.grab_focus()
+	fill(elizabeth_stats.battle_actions)
 	connect_scroll_children()
+
+	button_container.get_child(0).grab_focus()
 
 
 ###########
 ## METHODS
 ###########
+
+func fill(resource_list: Array[BattleAction]) -> void:
+	for resource in resource_list:
+		print(resource.name)
+		var resource_button: ResourceButton = add_resource_button()
+		resource_button.resource = resource
+		resource_button.text = resource.name
+
+
+func add_resource_button() -> ResourceButton:
+	var resource_button: ResourceButton = RESOURCE_BUTTON_SCENE.instantiate()
+	button_container.add_child(resource_button)
+	resource_button.resource_selected.connect(_on_resource_selected)
+	return resource_button
+
+
+func clear() -> void:
+	for button in button_container.get_children():
+		button.queue_free()
+
 
 func connect_scroll_children() -> void:
 	for button in button_container.get_children():
@@ -57,3 +86,6 @@ func _on_button_focused() -> void:
 		scroll_container, "scroll_vertical", focused_scroll_amount, 0.1
 	).from_current()
 
+
+func _on_resource_selected(resource: Resource) -> void:
+	print(resource.name)
