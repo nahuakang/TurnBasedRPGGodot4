26f39158f31a0fc67fcefe12579ae652a3c11ee7
Nahua Kang <kangnahua@gmail.com>
2023-06-11 20:31:21 +0200

    132: Implement basic `level_swapper` script

---
+++ scripts/level_swapper.gd
@@ -0,0 +1,30 @@
+extends Node
+class_name LevelSwapper
+
+#############
+## VARIABLES
+#############
+
+var player: CharacterBody2D
+
+###########
+## METHODS
+###########
+
+func stash_player(player_to_stash: CharacterBody2D) -> void:
+	player = player_to_stash
+	player.get_parent().remove_child(player)
+
+
+func drop_player() -> void:
+	await get_tree().process_frame
+	var parent := get_tree().current_scene
+	parent.add_child(player)
+	player.owner = parent
+
+
+func level_swap(player_to_stash: CharacterBody2D, new_level_string: String) -> void:
+	stash_player(player_to_stash)
+	get_tree().change_scene_to_file(new_level_string)
+	drop_player()
+
