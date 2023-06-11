953dcba721ca1c54ec6844125da662160110a999
Nahua Kang <kangnahua@gmail.com>
2023-06-11 21:11:21 +0200

    133: Set up `Door` for town and west meadow

--- scripts/level_swapper.gd
+++ scripts/level_swapper.gd
@@ -1,5 +1,4 @@
 extends Node
-class_name LevelSwapper
 
 #############
 ## VARIABLES
---
+++ world/door.gd
@@ -0,0 +1,9 @@
+extends Area2D
+class_name door
+
+#############
+## VARIABLES
+#############
+
+@export_file("*.tscn") var new_area
+@export_range(0, 32) var connection: int
