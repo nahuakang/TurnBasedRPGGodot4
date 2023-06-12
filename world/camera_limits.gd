92db1884379e6b9dac0cd99c7eba07745bb4f13d
Nahua Kang <kangnahua@gmail.com>
2023-06-12 22:14:24 +0200

    137: Overworld details (using a lot of y-sort on parents and children)

--- world/camera_limits.gd
+++ world/camera_limits.gd
@@ -1,3 +1,6 @@
+## NOTE: For initial game startup, `CameraLimits` must be placed below
+## `OverworldPlayer` so othat the `_ready` call order has already connected
+## the signal to a callback before `request_update_camera_limits.emit` is called.
 extends ColorRect
 class_name CameraLimits
 
